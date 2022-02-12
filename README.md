13677f67447438c08a738d8f0a2d93ce52eec68d
ikeman32 <david.isakson.ii@gmail.com>
2022-02-11 23:27:13 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Create-Browser-Extension
+Basic boilerplate for creating chrome and firefox extention. Requires developer account with chrome and/or firefox to publish.
+
+## install
+npx cbe
---
+++ cbe.js
@@ -0,0 +1,40 @@
+const fse = require("fs-extra");
+const readline = require("readline");
+const editJsonFile = require("edit-json-file");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What is the extention name? ", async function (answer) {
+    
+  rl.question("Enter a description ", async function (Description) {
+    const src = "./tmp/";
+    const dest = `./${answer}/`;
+    const ff = `./${answer}/firefox/manifest.json`;
+    const chrome = `./${answer}/chrome/manifest.json`;
+    // With promises
+    fse
+      .copy(src, dest)
+      .then(() => {
+        editManifest(ff, chrome, answer, Description);
+        console.log("Done");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+      rl.close();
+  });
+});
+
+const editManifest = (path, path2, name, description) => {
+  let file = editJsonFile(path);
+  file.set("name", name);
+  file.set("description", description);
+  file.save();
+  let file2 = editJsonFile(path2);
+  file2.set("name", name);
+  file2.set("description", description);
+  file2.save();
+};
